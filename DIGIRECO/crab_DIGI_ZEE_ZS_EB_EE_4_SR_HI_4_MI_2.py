cb8cc8de844845ef5ef7fa9d7d14378f5a29a9f2
Patricia <patricia.rebello.teles@cern.ch>
2023-09-09 11:06:33 +0200

    adding DIGI files

---
+++ DIGIRECO/crab_DIGI_ZEE_ZS_EB_EE_4_SR_HI_4_MI_2.py
@@ -0,0 +1,27 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+inputList='ZEEInputprebello.txt'
+
+config.General.requestName = 'ZEE_DIGI_ZS_EB_EE_4_SR_HI_4_MI_2'
+config.General.workArea = 'Ze10e10_pThat-0_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8_ZS_EB_EE_4_SR_HI_4_MI_2'
+config.General.transferOutputs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step2_DIGI_ZEE_ZS_EB_EE_4_SR_HI_4_MI_2.py'
+config.JobType.inputFiles = ["EcalSRSettings_beam2018_option1_v00_mc_ZS_EB_EE_4_SR_HI_4_MI_2.db"]
+
+config.Data.userInputFiles = open(inputList).readlines()
+config.Data.outputPrimaryDataset = 'Ze10e10_pThat-0_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8_ZS_EB_EE_4_SR_HI_4_MI_2'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+#NJOBS = 5000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line                            
+#config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.totalUnits = len(config.Data.userInputFiles)
+config.JobType.numCores = 8
+config.JobType.maxMemoryMB = 20000
+
+config.Data.publication = True
+config.Data.outputDatasetTag ='Ze10e10_pThat-0_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8_ZS_EB_EE_4_SR_HI_4_MI_2'
+config.Data.outLFNDirBase = '/store/user/prebello/'
+config.Site.storageSite ='T2_US_Vanderbilt'
